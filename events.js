e76a261b399b0f9e5f7c067f9d8bb06871380e02
Thando <ncubethando20@gmail.com>
2018-08-23 17:50:58 -0400

    Passing data using event emitters

--- events.js
+++ events.js
@@ -7,15 +7,15 @@ function Greetr() {
 
 util.inherits(Greetr, EventEmitter);
 
-Greetr.prototype.greet = function() {
-    console.log(this.greeting);
-    this.emit('greet');
-}
+Greetr.prototype.greet = function(data) {
+    console.log(this.greeting, ' ', data);
+    this.emit('greet', data);
+};
 
 var greeter1 = new Greetr();
 
-greeter1.on('greet', function() {
-    console.log("Someone greeted!");
-})
+greeter1.on('greet', function(data) {
+    console.log("Someone greeted!", data);
+});
 
-greeter1.greet();
+greeter1.greet('Thando');
